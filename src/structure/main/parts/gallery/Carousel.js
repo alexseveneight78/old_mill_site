24bdece7845956e6eea7aa65ed931a9bef572965
alexseveneight <alexseveneight@gmail.com>
2020-10-25 14:53:18 +0300

    'finished'

--- src/structure/main/parts/gallery/Carousel.js
+++ src/structure/main/parts/gallery/Carousel.js
@@ -15,6 +15,14 @@ import img11 from '../../../images/photos/11.jpg';
 import img12 from '../../../images/photos/12.jpg';
 import img13 from '../../../images/photos/13.jpg';
 import img14 from '../../../images/photos/14.jpg';
+import img15 from '../../../images/photos/15.jpg';
+import img16 from '../../../images/photos/16.jpg';
+import img17 from '../../../images/photos/17.jpg';
+import img18 from '../../../images/photos/18.jpg';
+import img19 from '../../../images/photos/19.jpg';
+import img20 from '../../../images/photos/20.jpg';
+import img21 from '../../../images/photos/21.jpg';
+import img22 from '../../../images/photos/22.jpg';
 
 
 export default function CarouselComponent() {
@@ -22,46 +30,70 @@ export default function CarouselComponent() {
         <div className="carousel-wrapper">
             <Carousel infiniteLoop useKeyboardArrows autoPlay>
                 <div>
-                    <img src={img1} alt="image" />
+                    <img src={img1} alt="image1" />
                 </div>
                 <div>
-                    <img src={img2} alt="image" />
+                    <img src={img2} alt="image2" />
                 </div>
                 <div>
-                    <img src={img3} alt="image" />
+                    <img src={img3} alt="image3" />
                 </div>
                 <div>
-                    <img src={img4} alt="image" />
+                    <img src={img4} alt="image4" />
                 </div>
                 <div>
-                    <img src={img5} alt="image" />
+                    <img src={img5} alt="image5" />
                 </div>
                 <div>
-                    <img src={img6} alt="image" />
+                    <img src={img6} alt="image6" />
                 </div>
                 <div>
-                    <img src={img7} alt="image" />
+                    <img src={img7} alt="image7" />
                 </div>
                 <div>
-                    <img src={img8} alt="image" />
+                    <img src={img8} alt="image8" />
                 </div>
                 <div>
-                    <img src={img9} alt="image" />
+                    <img src={img9} alt="image9" />
                 </div>
                 <div>
-                    <img src={img10} alt="image" />
+                    <img src={img10} alt="image10" />
                 </div>
                 <div>
-                    <img src={img11} alt="image" />
+                    <img src={img11} alt="image11" />
                 </div>
                 <div>
-                    <img src={img12} alt="image" />
+                    <img src={img12} alt="image12" />
                 </div>
                 <div>
-                    <img src={img13} alt="image" />
+                    <img src={img13} alt="image13" />
                 </div>
                 <div>
-                    <img src={img14} alt="image" />
+                    <img src={img14} alt="image14" />
+                </div>
+                <div>
+                    <img src={img15} alt="image15" />
+                </div>
+                <div>
+                    <img src={img16} alt="image16" />
+                </div>
+                <div>
+                    <img src={img17} alt="image17" />
+                </div>
+                <div>
+                    <img src={img18} alt="image18" />
+                </div>
+                <div>
+                    <img src={img19} alt="image19" />
+                </div>
+                <div>
+                    <img src={img20} alt="image20" />
+                </div>
+                <div>
+                    <img src={img21} alt="image21" />
+                </div>
+                <div>
+                    <img src={img22} alt="image22" />
                 </div>
             </Carousel>
         </div>
